a2f79b8d09edd1d02bd4c1eecfdd85d20d7fd541
Ratib Hoque <ratibhoque@gmail.com>
2026-07-13 12:14:14 +1000

    Installed NestJS

---
+++ frontend/Entertainment tracker/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ratib\\Desktop\\Entertainment_tracker_project\\frontend\\Entertainment tracker" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\ratib\\Desktop\\Entertainment_tracker_project\\frontend\\Entertainment tracker"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ratib\\Desktop\\Entertainment_tracker_project\\frontend\\Entertainment tracker\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ratib\\Desktop\\Entertainment_tracker_project\\frontend\\Entertainment tracker"
+  "FLUTTER_TARGET=C:\\Users\\ratib\\Desktop\\Entertainment_tracker_project\\frontend\\Entertainment tracker\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDQuNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049Zjk0ZjRmYzc2Yg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ODM2NzVlZDI3Ng==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMi4y"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ratib\\Desktop\\Entertainment_tracker_project\\frontend\\Entertainment tracker\\.dart_tool\\package_config.json"
+)

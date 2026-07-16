055badc02168971b6813f27a27dbbf97d74c20ff
Ratib Hoque <ratibhoque@gmail.com>
2026-07-16 16:41:23 +1000

    setup backend scaffolding - ready for primary dev and dev notes

--- backend/nest-api/src/app.module.ts
+++ backend/nest-api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { EntertainmentModule } from './entertainment/entertainment.module';
+import { LibraryModule } from './library/library.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { ListsModule } from './lists/lists.module';
+import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, EntertainmentModule, LibraryModule, ReviewsModule, ListsModule, StatisticsModule],
   controllers: [AppController],
   providers: [AppService],
 })
